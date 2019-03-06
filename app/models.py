c1743289d13201d063dc4c7314d78c2bd444562d
root <root@localhost.localdomain>
2019-03-07 04:14:07 +0800

    something new

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 class User(db.Model):
 	__tablename__='user'
@@ -8,3 +9,12 @@ class User(db.Model):
 	password_hash = db.Column(db.String(128))
 	def __repr__(self):
 		return '<username:{}>'.format(self.username)
+
+class Post(db.Model):
+	__tablename__='post'
+	id = db.Column(db.Integer,primary_key=True)
+	body = db.Column(db.String(140))
+	timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
+	user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+	def __repr__(self):
+		return '<Post{}>'.format(self.body)
--- config.py
+++ config.py
@@ -1,2 +1,7 @@
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
 	SECRET_KEY = 'gscwCOLD34#$'
+	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost:3306/flaskblog?charset=utf8'
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
